4ee73a93c01e8c7ee4d68547fc604631c5e9282a
AmyWentzel <144358028+AmyWentzel@users.noreply.github.com>
2025-06-09 16:35:23 -0400

    made the elevator buttons into actual buttons that submit the form

--- index.php
+++ index.php
@@ -35,7 +35,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../css/bootstrap.css" type="text/css" rel="stylesheet"/>
     <link href="css/elevator.css" type="text/css" rel="stylesheet"/>
     <title>Elevator Controller GUI</title>
 </head>
@@ -54,25 +53,21 @@
     ?>
 
     <h2 class="floor-display">F <?php echo $curFlr; ?></h2>
-    
-    <div>
-    </div>
-    <section class="elevator-panel">
-        
-            <div>
-                <!-- UP arrow: should INCREASE floor -->
-                <a type="submit" name="newfloor" value="<?php echo min(3, $curFlr+1); ?>" class="up" <?php if($curFlr >= 3) echo 'disabled'; ?> title="Up">&#8593;</a>
-            </div>
-            <div class="floor-row">
-                <?php for($i=1; $i<=3; $i++): ?>
-                    <a type="submit" name="newfloor" value="<?php echo $i; ?>" class="floor"<?php if($curFlr == $i) echo ' active'; ?>><?php echo $i; ?></a>
-                <?php endfor; ?>
-            </div>
-            <div>
-                <!-- DOWN arrow: should DECREASE floor -->
-                <a type="submit" name="newfloor" value="<?php echo max(1, $curFlr-1); ?>" class="down" <?php if($curFlr <= 1) echo 'disabled'; ?> title="Down">&#8595;</a>
-            </div>
-    </section>
+    <form  class="elevator-panel"method="post" action="index.php">
+        <div>
+            <!-- UP arrow: should INCREASE floor -->
+            <button type="submit" name="newfloor" value="<?php echo min(3, $curFlr+1); ?>" class="up" <?php if($curFlr >= 3) echo 'disabled'; ?> title="Up">&#8593;</button>
+        </div>
+        <div class="floor-row">
+            <?php for($i=1; $i<=3; $i++): ?>
+                <button type="submit" name="newfloor" value="<?php echo $i; ?>" class="floor"<?php if($curFlr == $i) echo ' active'; ?>><?php echo $i; ?></button>
+            <?php endfor; ?>
+        </div>
+        <div>
+            <!-- DOWN arrow: should DECREASE floor -->
+            <button type="submit" name="newfloor" value="<?php echo max(1, $curFlr-1); ?>" class="down" <?php if($curFlr <= 1) echo 'disabled'; ?> title="Down">&#8595;</button>
+        </div>
+    </form>
 </body>
 </html>
  
