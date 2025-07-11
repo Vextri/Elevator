882badf192dfe365917f45851410b2997f2df3ed
AmyWentzel <144358028+AmyWentzel@users.noreply.github.com>
2025-07-11 13:31:07 -0400

    test diagnostics.php

--- diagnostics/diagnostics.php
+++ diagnostics/diagnostics.php
@@ -1,5 +1,5 @@
 <?php
-    $str = file_get_contents('json/diagnostics.json');
+    $str = file_get_contents('../json/diagnostics.json');
     $json = json_decode($str, true);
     echo '<pre>' . print_r($json, true) . '</pre>';
     exit;
