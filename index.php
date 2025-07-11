9c97d7c06de794fb0bf66464d68ec0fa6e32816d
AmyWentzel <144358028+AmyWentzel@users.noreply.github.com>
2025-07-11 16:02:36 -0400

    small fixes for GUI

--- index.php
+++ index.php
@@ -13,6 +13,18 @@
         return $new_floor;
     }
 
+    function get_currentFloor(): int {
+		try { $db = new PDO('mysql:host=127.0.0.1;dbname=elevator','ese','ese');}
+		catch (PDOException $e){echo $e->getMessage();}
+
+			// Query the database to display current floor
+			$rows = $db->query('SELECT currentFloor FROM elevatorNetwork');
+			foreach ($rows as $row) {
+				$current_floor = $row[0];
+			}
+			return $current_floor;
+	}
+
 ?>
 
 <html>
